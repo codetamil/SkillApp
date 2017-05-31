2f9b41c59695028cc728909b1efdbd572cf43d74
codetamil <sairam.s2002@gmail.com>
2017-05-31 12:43:40 +0530

    changes
    changes added

---
+++ skillapp/src/main/java/com/revature/skillapp/SkillApp/SkillAppApplication.java
@@ -0,0 +1,12 @@
+package com.revature.skillapp.SkillApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillAppApplication.class, args);
+	}
+}
